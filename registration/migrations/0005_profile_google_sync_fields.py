ef912a83141a5bab8d4be04a66b5f19db99986c2
kmorri09 <kevin@ruoomsoftware.com>
2026-07-29 15:35:12 -0500

    Make profile migration SQLite compatible

--- registration/migrations/0005_profile_google_sync_fields.py
+++ registration/migrations/0005_profile_google_sync_fields.py
@@ -8,31 +8,14 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.SeparateDatabaseAndState(
-            database_operations=[
-                migrations.RunSQL(
-                    sql="""
-                        ALTER TABLE registration_profile
-                        ADD COLUMN IF NOT EXISTS google_sync_enabled boolean NOT NULL DEFAULT false;
-                        ALTER TABLE registration_profile
-                        ALTER COLUMN google_sync_enabled SET DEFAULT false;
-                        ALTER TABLE registration_profile
-                        ADD COLUMN IF NOT EXISTS google_credentials_json text;
-                    """,
-                    reverse_sql=migrations.RunSQL.noop,
-                )
-            ],
-            state_operations=[
-                migrations.AddField(
-                    model_name="profile",
-                    name="google_sync_enabled",
-                    field=models.BooleanField(default=False),
-                ),
-                migrations.AddField(
-                    model_name="profile",
-                    name="google_credentials_json",
-                    field=models.TextField(blank=True, null=True),
-                ),
-            ],
+        migrations.AddField(
+            model_name="profile",
+            name="google_sync_enabled",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name="profile",
+            name="google_credentials_json",
+            field=models.TextField(blank=True, null=True),
         ),
     ]
